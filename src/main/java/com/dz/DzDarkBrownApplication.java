8d468799c2b08803aa9bbdac21ee14ae233ff195
Kimyongjin <yongjin5184@gmail.com>
2018-10-25 09:27:08 +0900

    프로젝트생

---
+++ src/main/java/com/dz/DzDarkBrownApplication.java
@@ -0,0 +1,12 @@
+package com.dz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzDarkBrownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzDarkBrownApplication.class, args);
+	}
+}
